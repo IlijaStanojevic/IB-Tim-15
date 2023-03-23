43a754c2642769ea6ea9e39753bf18d25d0543b4
REX1389 <rrvlelcc@gmail.com>
2023-03-23 22:52:08 +0100

    Creating CertificateRefRepository

---
+++ IB-Backend/src/main/java/com/example/ibbackend/repository/CertificateRefRepository.java
@@ -0,0 +1,9 @@
+package com.example.ibbackend.repository;
+
+import com.example.ibbackend.model.CertificateRef;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CertificateRefRepository extends JpaRepository<CertificateRef, Long>
+{
+
+}
