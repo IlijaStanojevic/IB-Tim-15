cc4d78a802e2180a4a863c27e7d5604290a89685
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-03-31 15:05:29 +0200

    Add jwt filter and implement userdetails methods

--- IB-Backend/src/main/java/com/example/ibbackend/model/Admin.java
+++ IB-Backend/src/main/java/com/example/ibbackend/model/Admin.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Admin extends User{
-	public Admin(String id, String email, String name, String surname, String phoneNumber, String password) {
-		super(id, email, name, surname, phoneNumber, password);
+	public Admin(String id, String email, String password, String phoneNum, String name, String surname) {
+		super(id, email, name, surname, phoneNum, password);
 	}
 }
