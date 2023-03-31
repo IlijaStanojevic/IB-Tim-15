f54b9a5bee254b0767514c9db1c3a13276d699f8
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-03-31 13:57:01 +0200

    Add login and register

--- IB-Backend/src/main/java/com/example/ibbackend/IbBackendApplication.java
+++ IB-Backend/src/main/java/com/example/ibbackend/IbBackendApplication.java
@@ -1,6 +1,8 @@
 package com.example.IBBackend;
 
+import com.example.IBBackend.dto.UserRequest;
 import com.example.IBBackend.model.User;
+import com.example.IBBackend.repository.UserRepository;
 import com.example.IBBackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,7 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-public class IbBackendApplication  {
+public class IbBackendApplication{
 //	@Autowired
 //	private UserService userService;
 	public static void main(String[] args) {
@@ -17,13 +19,10 @@ public class IbBackendApplication  {
 
 //	@Override
 //	public void run(String... args) throws Exception {
-//		userService.save(new User(1, "AAAAAAAAAAAAAAAAAAAA@gma", "123", "Pera", "Peric"));
-//		userService.save(new User(2, "aaaaaaaa@gma", "123", "Djura", "Djuric"));
+//		userService.save(new UserRequest("1", "AAAAAAAAAAAAAAAAAAAA@gma", "123", "Pera", "Peric"));
+//		userService.save(new UserRequest("2", "aaaaaaaa@gma", "123", "Djura", "Djuric"));
 //		for (User user : userService.findAll()) {
 //			System.out.println(user);
 //		}
-//
-//		System.out.println(userService.loadUserByUsername("aaaaaaaa@gma"));
-//
 //	}
 }
