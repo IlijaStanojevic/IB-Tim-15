c756279fd971e79f7843a4d75170c386f24a3034
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-03-31 13:55:37 +0200

    Add methods for adding users

---
+++ IB-Backend/src/main/java/com/example/ibbackend/dto/LoginResponse.java
@@ -0,0 +1,48 @@
+package com.example.IBBackend.dto;
+
+import java.util.Date;
+
+public class LoginResponse {
+    private String accessToken;
+
+
+    private String userId;
+    private Date expiresIn;
+
+    public LoginResponse() {
+        this.accessToken = null;
+        this.expiresIn = null;
+    }
+
+    public LoginResponse(String accessToken, Date expiresIn) {
+        this.accessToken = accessToken;
+        this.expiresIn = expiresIn;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Date getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Date expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+
+}
--- IB-Backend/src/main/java/com/example/ibbackend/service/UserService.java
+++ IB-Backend/src/main/java/com/example/ibbackend/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.IBBackend.service;
 
+import com.example.IBBackend.dto.UserRequest;
 import com.example.IBBackend.model.User;
 import com.example.IBBackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +18,8 @@ import java.util.Optional;
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public Optional<User> getByEmail(String email){
         return userRepository.findUserByEmail(email);
     }
@@ -33,8 +38,17 @@ public class UserService implements UserDetailsService {
         throw new UsernameNotFoundException("User not found with this email: " + email);
     }
 
-    public void save(User user) {
-        userRepository.save(user);
+    public User save(UserRequest userRequest) {
+        User u = new User();
+//        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        u.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+//        u.setPassword(encoder.encode(userRequest.getPassword()));
+        u.setName(userRequest.getName());
+        u.setSurname(userRequest.getSurname());
+        u.setEmail(userRequest.getEmail());
+        u.setPhoneNum(userRequest.getPhoneNum());
+
+        return userRepository.save(u);
     }
 
     public List<User> findAll() {
