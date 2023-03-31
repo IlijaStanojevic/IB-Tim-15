1e17eab40b0c80385aff41124601ce96ba973fe7
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-03-31 16:27:21 +0200

    Reformat

--- IB-Backend/src/main/java/com/example/ibbackend/repository/CertificateRepository.java
+++ IB-Backend/src/main/java/com/example/ibbackend/repository/CertificateRepository.java
@@ -5,11 +5,9 @@ import com.example.IBBackend.model.Certificate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface CertificateRefRepository extends MongoRepository<Certificate, Long>
+public interface CertificateRepository extends MongoRepository<Certificate, Long>
 {
-    
+
 
 }
--- IB-Backend/src/main/java/com/example/ibbackend/service/CertificateGenerator.java
+++
@@ -1,4 +0,0 @@
-package com.example.IBBackend.service;
-
-public class CertificateGenerator {
-}
