109df351a376857ddc648f04b7ee5999573ea2e1
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-05-10 02:30:54 +0200

    Add downloading keys

--- IB-Frontend/src/app/my-certs/my-certs.component.ts
+++ IB-Frontend/src/app/my-certs/my-certs.component.ts
@@ -3,6 +3,7 @@ import {Certificate} from "../all-certs/all-certs.component";
 import {CertificateService} from "../service/certificate.service";
 import {Validators} from "@angular/forms";
 import {firstValueFrom} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-my-certs',
@@ -11,7 +12,7 @@ import {firstValueFrom} from "rxjs";
 })
 export class MyCertsComponent implements  OnInit{
   certificates: Certificate[] = [];
-  constructor(private certificateService: CertificateService) {
+  constructor(private certificateService: CertificateService, private http: HttpClient) {
   }
   ngOnInit() {
     this.loadCertificates();
@@ -29,5 +30,32 @@ export class MyCertsComponent implements  OnInit{
     if (!!response) {
       this.certificates = response;
     }
+  }
+
+  downloadKey(serialNumber: string) {
+    const url = `http://localhost:8080/api/keys/${serialNumber}/download`;
+
+    // Set the response type to 'blob' for downloading files
+    const options = { responseType: 'blob' as 'json' };
+
+    this.http.get(url, options).subscribe((data: any) => {
+      // Create a Blob from the response data
+      const blob = new Blob([data], { type: 'application/octet-stream' });
+
+      // Generate a unique file name for the downloaded file
+      const fileName = `${serialNumber}.key`;
+
+      // Create a temporary link element
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = fileName;
+
+      // Programmatically click the link to trigger the download
+      link.click();
+
+      // Clean up the temporary link
+      window.URL.revokeObjectURL(link.href);
+      link.remove();
+    });
   }
 }
